0d4824726f31bd8815e02f95f4a33a6bcfb74b4f
devadilson <didasoueudida@gmail.com>
2021-11-10 14:46:02 -0300

    Mart northwind

--- models/northwind/mart/dim_supplier.sql
+++ models/northwind/mart/dim_supplier.sql
@@ -1,74 +1,28 @@
 {{ config(materialized='table') }}
 
-with 
-    customers as (
+with
+    staging as (
+        select *
+        from {{ ref('stg_suppliers') }}
+)
+    , transformed as (
         select
-        customer_sk
-        ,customer_id
-        ,customer_name
-        from {{ref('dim_customers')}}
-    )
 
-, products as (
-        select
-        product_id
-        ,product_name
-        from {{ref('dim_product')}}
-    )
 
-, orders_with_sk as (
-        select
-        orders.order_id
-        , customers.customer_id
-        , customer_name
-        , orders.order_date
-        , orders.ship_region
-        , orders.shipped_date
-        , orders.ship_country
-        , orders.ship_address
-        , orders.ship_postal_code
-        , orders.ship_city
-        , orders.ship_name
-        , orders.freight
-        , orders.required_date 
-
-    from {{ref('stg_orders')}} orders
-    left join customers customers on orders.customer_id = customers.customer_id
+           row_number() over (order by supplier_id) as supplier_sk -- auto incremental surrogate key
+           ,supplier_id		
+           ,supplier_name		
+           ,contact_name		
+           ,contact_title		
+           ,address		
+           ,city		
+           ,region		
+           ,postal_code		
+           ,country		
+           ,phone		
+           ,fax		
+           ,homepage	
+        from staging     
 )
 
-, shippers as (
-        select
-        shipper_sk
-        ,shipper_id
-        ,shipper_name
-        from {{ref('dim_shipper')}}
-)
-
-, suppliers as (
-        select
-        supplier_sk
-        ,supplier_id
-        ,supplier_name
-        from {{ref('dim_supplier')}}
-)
-
-select  order_id
-        , customer_id
-        , customer_name
-        , product_id
-        , product_name
-        , order_date
-        , shipper_id
-        , shipper_name
-        , ship_region
-        , shipped_date
-        , ship_country
-        , ship_address
-        , ship_postal_code
-        , ship_city
-        , ship_name
-        , freight
-        , required_date 
-        , supplier_id
-        , supplier_name
-from products, orders_with_sk, shippers, suppliers
+select * from transformed
