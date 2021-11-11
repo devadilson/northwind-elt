899cfdfa9215a4e6d4e8aebd92b5af61e7e0a2ed
devadilson <didasoueudida@gmail.com>
2021-11-11 16:53:07 -0300

    Ordem detail

--- models/northwind/mart/fact_order.sql
+++ models/northwind/mart/fact_order.sql
@@ -77,8 +77,7 @@ with
         left join employees employees on orders.employee_id = employees.employee_id
         left join customers customers on orders.customer_id = customers.customer_id
         left join shippers shippers on orders.shipper_id = shippers.shipper_sk
-        left join order_detail order_detail on order_detail.order_id = orders.order_id
-        left join order_detail order_detail on order_detail.product_id = products.product_id
+        left join order_detail order_detail on order_detail.order_id = orders.order_id and order_detail.product_id = products.product_id
     )
 
     select * from orders_with_sk
